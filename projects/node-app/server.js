d992211468c3d78b0309814a30093889289ba9d0
BBiiaoao <511299371@qq.com>
2018-10-23 15:44:05 +0800

    zb

---
+++ projects/node-app/server.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const app=express();
+
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+})
+
+const port=process.env.PORT||5000;
+
+app.listen(port,()=>{
+    console.log(`Serve running on port ${port}`);
+})
