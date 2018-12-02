35232c5a11c168e9563fea09015b75e5f57adc04
829_100008418348_BBiiaoao <829_100008418348_BBiiaoao@git.cloud.tencent.com>
2018-12-02 11:36:34 +0800

    zb

--- projects/node-app/client/src/views/Index.vue
+++ projects/node-app/client/src/views/Index.vue
@@ -23,5 +23,6 @@
         width: 100%;
         height: 100%;
         overflow: hidden;
+        background-color: rgba(0, 0, 0, 0.1);
     }
 </style>
--- projects/node-app/client/src/views/Infoshow.vue
+++ projects/node-app/client/src/views/Infoshow.vue
@@ -39,32 +39,56 @@
 
 <style scoped>
     .infoshow{
-        height: 100%;
+        margin:200px auto 0;
+        width: 380px;
+        height: 240px;
+        border-radius: 15px;
+        box-shadow:-2px 2px 0px 0px #202020, -10px 10px 40px 0px rgba(0, 0, 0, 0.3);
+        border: 2px solid #F26B3A;
+        color:#ddd;
+        background-color:#303030 ;
+        transform: translate(0px, 0px) rotateX(15deg) rotateZ(-5deg);
+        animation:upFloat 2s ease-in-out infinite alternate;
+        transition: all 1s;
+    }
+    @keyframes upFloat{
+        from{transform: translate(0px, 0px) rotateX(15deg) rotateZ(-5deg)}
+        to{
+            transform: translate(0px, -20px) rotateX(0deg) rotateZ(-5deg);
+            box-shadow: -2px 1px 0px 0px #202020, -40px 40px 40px 0 rgba(0, 0, 0, 0.2);
+        }
+    }
+    .infoshow:hover{
+        animation-play-state: paused;
     }
     .row-bg{
         width: 100%;
         height: 100%;
     }
     .user{
-        padding-top: 200px;
         width: 100%;
         height: 100%;
         text-align: right;
     }
     .avatar{
-        width: 100px;
-        height: 100px;
-        margin-right: 20px;
+        width: 120px;
+        height: 120px;
+        margin: 60px 0 0 10px;
+        border-radius: 50%;
+        overflow: hidden;
     }
     .userinfo{
-        padding-top: 200px;
         width: 100%;
         height: 100%;
-        background-color: #ddd;
+        padding-top: 70px;
+        margin-left: 20px;
     }
     .user-item{
-        margin: 10px 0 10px 15px;
+        margin-top: 10px;
         font-size: 25px;
         font-weight: 600;
+    }
+    .icon{
+        margin-right: 10px;
     }
 </style>
